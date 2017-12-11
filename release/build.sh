84837e9cfa1348a85b3cf667246c82945fb60960
Arthur Chaloin <arthur.chaloin@epitech.eu>
2017-12-11 17:51:38 +0100

    migrate to v1.0.0

---
+++ release/build.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+version="1.0.0"
+
+cd ../
+
+echo "===== BUILDING IOS RELEASE ======"
+
+ionic cordova build ios --prod
+
+
+echo "===== BUILDING ANDROID RELEASE ====="
+
+ionic cordova build android --prod --release
+
+apk="platforms/android/build/outputs/apk/android-release-unsigned.apk"
+signed="release/android/poloniex-v"$version"_signed.apk"
+
+rm $signed
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release/android/arthurchaloin.jks $apk chaloin
+~/Library/Android/sdk/build-tools/25.0.3/zipalign -v 4 $apk $signed
+~/Library/Android/sdk/build-tools/25.0.3/apksigner verify $signed
